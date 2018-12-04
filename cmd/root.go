ff44a2eeba49c29c953243304b319b15194a5f03
lylex <xuqianzhou@gmail.com>
2018-12-04 16:51:20 +0800

    add configuration file via viper

--- cmd/root.go
+++ cmd/root.go
@@ -2,19 +2,24 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/lylex/drm/pkg/files"
 	"github.com/lylex/drm/pkg/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 const (
 	// RootCmdName represents the cobra root command name.
 	RootCmdName = "drm"
 
-	// TODO replate it
+	// DefaultConfigPath represents the default path for config.
+	DefaultConfigPath = "/etc/drm/drm.cfg"
+
+	// TempFileStorePath TODO replate it
 	TempFileStorePath = "/Users/xuq3/workspace/drm/tem"
 )
 
@@ -22,13 +27,21 @@ var (
 	isForce     bool
 	isRecursive bool
 	version     string
+	cfgFile     = DefaultConfigPath
 )
 
 func init() {
-	RootCmd.Flags().BoolVarP(&isRecursive, "recursive", "r", false, `ignore
-nonexistent files and arguments, never prompt`)
-	RootCmd.PersistentFlags().BoolVarP(&isForce, "force", "f", false, `remove directories
-and their contents recursively or not`)
+	cobra.OnInitialize(initConfig)
+
+	// determin whether to remove recursively.
+	RootCmd.Flags().BoolVarP(&isRecursive, "recursive", "r", false, "remove directories and their contents recursively or not")
+
+	// determine whether to remove objects by force.
+	RootCmd.Flags().BoolVarP(&isForce, "force", "f", false, "ignore nonexistent files and arguments, never prompt")
+
+	// pass configuration file from options.
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", DefaultConfigPath,
+		fmt.Sprintf("config file (default is %s)", DefaultConfigPath))
 }
 
 // RootCmd represents the base command.
@@ -45,6 +58,13 @@ var RootCmd = &cobra.Command{
 	Version: fmt.Sprintf("%s", version),
 	Args:    cobra.ArbitraryArgs,
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			msg := "no operation concluded"
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("Error: %s\n", msg))
+			cmd.Usage()
+			utils.ErrExit(fmt.Sprintf("\nFailed to execute command: %s\n", msg), nil)
+		}
+
 		for _, path := range args {
 			if !files.IsAbsolutePath(path) {
 				path = filepath.Join(files.GetWd(), path)
@@ -60,7 +80,7 @@ var RootCmd = &cobra.Command{
 			if files.IsDir(path) && !isRecursive {
 				utils.ErrExit(fmt.Sprintf("%s: %s: is a directory\n", RootCmdName, path), nil)
 			}
-			files.Move(path, filepath.Join(TempFileStorePath,
+			files.Move(path, filepath.Join(string(viper.GetString("dataPath")),
 				fmt.Sprintf("%d_%s", time.Now().UnixNano(), files.Name(path))))
 		}
 	},
@@ -72,4 +92,20 @@ func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		utils.ErrExit("Failed to execute command: %+v\n", err)
 	}
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile == "" {
+		cfgFile = DefaultConfigPath
+	}
+	viper.SetConfigFile(cfgFile)
+	viper.SetConfigType("json")
+
+	if err := viper.ReadInConfig(); err != nil {
+		utils.ErrExit("Failed to read configuration file: %+v\n", err)
+	}
+
+	// read config from enviroment virable.
+	viper.AutomaticEnv()
 }
